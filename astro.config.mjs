d4008710ab9701c0ac40a8445d4f18c77c4cfb4a
Ramiro Cerdá <rami992009@gmail.com>
2025-02-24 22:53:34 -0300

    feat(ui): add minimalist style to the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import preact from "@astrojs/preact";
 export default defineConfig({
   site: "https://ramirocerda.vercel.app",
   integrations: [preact()],
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
 });
